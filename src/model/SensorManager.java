3eaef733ccc2668298ca286654bfc6a31ae59f3c
Eduardo <edumadrim@gmail.com>
2019-10-08 16:39:10 -0600

    SensorManager Add, Delete, Load Methods

--- src/model/SensorManager.java
+++ src/model/SensorManager.java
@@ -8,19 +8,33 @@ public class SensorManager {
 	private NaryTree<Sensor> mainTree;
 	private SplayTree<NaryTreeNode<Sensor>> splayTree;
 
-	public SensorManager(ArrayList<Sensor> pInitialSensorList) {
+	public SensorManager() {
 		CreateSensorTree();
 		splayTree = new SplayTree<NaryTreeNode<Sensor>>();
-		
-
 	}
-
-	//public void Load
 
 	private void CreateSensorTree() {
 		mainTree = new NaryTree<Sensor>(new NaryTreeNode<Sensor>(new Sensor("Planta")));
 	}
+	//Solo carga en la raiz del arbol
+	public void LoadSensors(ArrayList<Sensor> pInitialSensorList){ 
+		for (Sensor sensor: pInitialSensorList) {
+			AddSensor(sensor, mainTree.getRoot());
+		}
+	}
 
+	public void AddSensor(Sensor pSensor,NaryTreeNode<Sensor> pParentNaryNode){
+		NaryTreeNode<Sensor> node = new NaryTreeNode<Sensor>(pSensor);
+		mainTree.AddTo(pParentNaryNode, node);
+		splayTree.add(node);
+	}
+
+	public void DeleteSensor(NaryTreeNode<Sensor> pNode){
+		mainTree.DeleteNode(pNode);
+		//splayTree.deleteNode();
+	}
+	//Funciones de buscar
+	//Recorridos
 	public void ChangeValues() {
 		ChangeValuesAux(mainTree.getRoot());
 	}
@@ -34,4 +48,13 @@ public class SensorManager {
 		;
 		pNode.getValue().setIntake(intake);
 	}
+
+	public void CheckWaterFlow(){
+
+	}
+
+	public void CheckWaterFlowAux(NaryTreeNode<Sensor> pNode){
+
+	}
+
 }
